4457c775a4d802ce48ea16e868394a239969d2e9
alisher_ashyrov <alisher_ashyrov@mail.ru>
2021-07-25 00:57:25 +0300

    feat: append sqlite db source files

--- database/database.hpp
+++ database/database.hpp
@@ -4,5 +4,12 @@
 
 #ifndef INVENTORY_TEST_DATABASE_DATABASE_HPP_
 #define INVENTORY_TEST_DATABASE_DATABASE_HPP_
+#include <QString>
+
+class Database {
+ public:
+  virtual bool connect() noexcept = 0;
+  virtual bool disconnect() noexcept = 0;
+};
 
 #endif //INVENTORY_TEST_DATABASE_DATABASE_HPP_
