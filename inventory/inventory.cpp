be11a7cc6afdf91abacf5d82b66b1a42d01ea336
alisher_ashyrov <alisher_ashyrov@mail.ru>
2021-07-25 00:50:03 +0300

    feat: append inventory getters

--- inventory/inventory.cpp
+++ inventory/inventory.cpp
@@ -2,3 +2,16 @@
 // Created by alisher on 7/24/21.
 //
 
+#include "inventory.hpp"
+
+Inventory::Inventory(qsizetype size) : size_{size} {
+  items_.reserve(size);
+}
+
+qsizetype Inventory::size() const noexcept {
+  return size_;
+}
+
+const QVector<QPair<Item::Type, qsizetype>> &Inventory::items() const noexcept {
+  return items_;
+}
--- inventory/inventory.hpp
+++ inventory/inventory.hpp
@@ -4,5 +4,21 @@
 
 #ifndef INVENTORY_TEST_INVENTORY_INVENTORY_HPP_
 #define INVENTORY_TEST_INVENTORY_INVENTORY_HPP_
+#include <QMap>
+#include <QVector>
+#include <QString>
+#include "../item/item.hpp"
+
+class Inventory {
+ public:
+  explicit Inventory(qsizetype size);
+
+  ~Inventory() = default;
+  qsizetype size() const noexcept;
+  const QVector<QPair<Item::Type, qsizetype>> &items() const noexcept;
+ private:
+  qsizetype size_;
+  QVector<QPair<Item::Type, qsizetype>> items_;
+};
 
 #endif //INVENTORY_TEST_INVENTORY_INVENTORY_HPP_
