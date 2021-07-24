42a5b5f9a915706a8225ebd5529c704d1e113b14
alisher_ashyrov <alisher_ashyrov@mail.ru>
2021-07-24 21:31:34 +0300

    feat: update project structure

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
+  QApplication app(argc, argv);
   Widget w;
   w.show();
 
-  return a.exec();
+  return app.exec();
 }
