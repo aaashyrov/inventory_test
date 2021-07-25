2bcd8a81e81ec0f097e9b19f4c72e09dab948e9d
alisher_ashyrov <alisher_ashyrov@mail.ru>
2021-07-25 18:04:53 +0300

    refactor: delete extra semicolon

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     QMessageBox message_box;
     message_box.setText(mainwindow.message());
     return message_box.exec();
-  };
+  }
 
   mainwindow.show();
 
