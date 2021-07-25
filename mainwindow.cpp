a96cfd51475c5772996486028b2d6351e5acfb0b
alisher_ashyrov <alisher_ashyrov@mail.ru>
2021-07-25 18:41:36 +0300

    refactor: update gui initialization

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,30 +5,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <database/sqlitedb.hpp>
+#include <iostream>
 
 constexpr unsigned int count = 3;
 
-MainWindow::MainWindow(QWidget *parent) : QWidget(parent), ui_{std::make_unique<Ui::Widget>()} {}
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent), ui_{std::make_unique<Ui::Widget>()}, controller_{std::make_unique<Controller>()} {}
 
 bool MainWindow::initialize(int argc, char **argv) noexcept {
-  database_ = std::make_shared<SqliteDb>(Param{"localhost", "inventory", "user", "pass"});
+  database_ = std::make_shared<SqliteDb>(Param{"127.0.0.1", "/home/alisher/inventory.db", "root", ""});
   if (not database_->isOpen()) {
     message_ = database_->message();
     return false;
   }
 
+  if (not controller_->initialize(database_)) {
+    return false;
+  }
+
+
   ui_->setupUi(this);
+
   ui_->gameWidget->hide();
-  ui_->inventoryTableWidget->setRowCount(count);
+
   ui_->inventoryTableWidget->setColumnCount(count);
   ui_->inventoryTableWidget->verticalHeader()->hide();
   ui_->inventoryTableWidget->horizontalHeader()->hide();
-  auto cellWidth = ui_->inventoryTableWidget->width() / count - 1;
-  auto cellHeight = ui_->inventoryTableWidget->height() / count - 1;
+  ui_->inventoryTableWidget->setRowCount(controller_->inventory().size() / count);
+
+  auto cellsize = ui_->inventoryTableWidget->width() / count - 1;
   for (std::size_t i = 0; i < count; ++i) {
-    ui_->inventoryTableWidget->setColumnWidth(i, cellWidth);
-    ui_->inventoryTableWidget->setRowHeight(i, cellHeight);
+    ui_->inventoryTableWidget->setColumnWidth(i, cellsize);
+    ui_->inventoryTableWidget->setRowHeight(i, cellsize);
   }
+
+  ui_->itemTableWidget->verticalHeader()->hide();
+  ui_->itemTableWidget->setColumnCount(1);
+  ui_->itemTableWidget->horizontalHeader()->hide();
+  ui_->itemTableWidget->setRowCount(controller_->items().size());
+
+  for (std::size_t i = 0; i < 2; ++i) {
+    ui_->itemTableWidget->setRowHeight(i, cellsize);
+    ui_->itemTableWidget->setColumnWidth(i, cellsize);
+  }
+
   return true;
 }
 
