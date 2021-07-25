5ced5c477df00977def44a285835cc3ac555743a
alisher_ashyrov <alisher_ashyrov@mail.ru>
2021-07-25 18:03:01 +0300

    feat: update databas interface

--- database/database.hpp
+++ database/database.hpp
@@ -4,12 +4,18 @@
 
 #ifndef INVENTORY_TEST_DATABASE_DATABASE_HPP_
 #define INVENTORY_TEST_DATABASE_DATABASE_HPP_
+#include <QMap>
 #include <QString>
+#include <QVariant>
 
 class Database {
  public:
   virtual bool isOpen() noexcept = 0;
   virtual QString message() noexcept = 0;
+
+  virtual qsizetype size_of(const QString &table) noexcept = 0;
+  virtual std::optional<QVariant> value(const QString &table, const QString &name, qsizetype i) noexcept = 0;
+
   virtual ~Database() = default;
 };
 
